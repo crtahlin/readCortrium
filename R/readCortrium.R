0be2ab0f08eeeb6ba5346330338651068be931e5
Crt Ahlin <crt.ahlin@gmail.com>
2014-10-16 16:54:18 +0200

    Added a simple function for calculating heart beat times from ECG.

--- R/readCortrium.R
+++ R/readCortrium.R
@@ -38,11 +38,16 @@ readDegrees <- function (dirname) {
 #' greater than 32676 - in the matlab scripts they seem to be set to 0.
 #' 
 #' @param dirname The path to the directory containing the ecgX_raw.bin files.
+#' @param recordType The type of recorded signal to read - 
+#' "raw" (data only high-pass filtered at 0.3Hz) or 
+#' "filtered" (data further low-passs filtered at 40Hz). Defaults to "raw".
 #' @export
-readECG <- function (dirname) {
+readECG <- function (dirname, recordType="raw") {
   # for each ECG lead
   for (i in 1:3) {
-    filename <- paste0(dirname, "ecg", i, "_raw.bin")
+    if (recordType=="raw") {filename <- paste0(dirname, "ecg", i, "_raw.bin")}
+    if (recordType=="filtered") {filename <- paste0(dirname, "ecg", i, "_filt.bin")}  
+    
     fileSize <- file.info(filename)$size
     # open a connection to the file
     con <- file(filename, open="rb")
@@ -79,4 +84,49 @@ readRespiration <- function (dirname) {
   close(con)
   # return respiration rate records
   return(data.frame(respRate))
+}
+
+#' @title A simple function to extract heart beat timings from ECG data
+#' 
+#' @description Reads the ECG signal data and applies a highpass filter to 
+#' exagurate the spikes of the QRS complex. Than looks where the spikes are higher
+#' than some value and assumes that represents the R part of the ECG. Looks
+#' where the next peak is more than 25 values (100ms) away from the previous one
+#' and assumes this as the start of the next heart beat. Uses the
+#' ECG1 records. This is a simple method - better ones exist - 
+#' for a quick implementation.
+#' Outputs data frame with columns representing beat times in seconds. 
+#' 
+#' @param dirname The path to the directory containing the ecgX_raw.bin files.
+#' @export
+beatsFromECG <- function (dirname, cutoffValue=200, highpassFreq=0.3) {
+  # read the ECG values
+  ECG <- readECG(dirname, recordType = "raw")
+  # load library for manipulating signals
+  library(signal)
+  # apply high pass filter to the data
+  highpass <- butter(2, highpassFreq, type="high")
+  ECGfiltered <- filter(highpass, x=ECG$ECG1)
+  
+  # extract high values
+  highValues <- which(ECGfiltered>cutoffValue)
+  # we do 250Hz sampling and the QRS complex takes up to 0.1s 
+  # (http://en.wikipedia.org/wiki/Electrocardiography)
+  # so let's combine the values within this window into one value
+  # 0.1s = 100ms = 25 consecutive measurements
+  isBeat <- vector()
+  for (i in 2:length(highValues)) {
+    # the first high value is presumably a "heart beat"
+    isBeat[1] <- TRUE
+    # the following high values are compared to the previous value
+    # browser()
+    isBeat[i] <- highValues[i] > (highValues[i-1] + 25)
+  }
+  
+  # calcluate beat times for records marked as new beats
+  beatTimes <- highValues[isBeat==TRUE]/250
+  
+  # return a data frame of beat times in seconds
+  return(as.data.frame(beatTimes))
+  
 }
